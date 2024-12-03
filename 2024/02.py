6ed1b5506dabbe7cfe11d7412d8b1ef14873436b
Oriol Sastre <oriol.sastre@gmail.com>
2024-12-03 11:45:34 +0100

    03

--- 2024/02.py
+++ 2024/02.py
@@ -3,25 +3,16 @@ file = "02_data.txt"
 safeReport = 0
 safeReportTolerance = 0
 
-def reportSafety(report, tolerance=0)->bool:
+def report_safety(report, tolerance=0)->bool:
   signe = math.copysign(1, report[0] - report[1])
   if signe == 0:
-    # if tolerance == 0:
-    #   return reportSafety(report[1:], 1)
     return False
   for i in range(len(report)-1):
     diff = report[i] - report[i+1]
     level_sign = math.copysign(1, diff)
-    # report_minus_i = report[:i+1]
-    # if i < len(report)-2:
-    #   report_minus_i = report[:i+1]+report[i+2:]
     if level_sign != signe:
-      # if tolerance == 0:
-      #   return reportSafety(report_minus_i, 1)
       return False
     if abs(diff) < 1 or abs(diff) > 3:
-      # if tolerance == 0:
-      #   return reportSafety(report_minus_i, 1)
       return False
   return True
 
@@ -32,20 +23,15 @@ with open(file, "r") as reports:
     report = report.split(" ")
     # Convertim els nivells a enters
     report = [int(x) for x in report]
-    if reportSafety(report):
+    if report_safety(report):
       safeReport += 1
       safeReportTolerance += 1
     else:
       for i in range(len(report)):
         newReport = report[:i] + report[i+1:]
-        if reportSafety(newReport):
+        if report_safety(newReport):
           safeReportTolerance += 1
           break
-      # if reportSafety(report):
-      #   safeReport += 1
-      # else:
-      #   if reportSafety(report[1:], 1):
-      #     safeReport += 1
 
 print("Reports valids que hi ha:", safeReport) #202
 print("Reports valids amb tolerència:", safeReportTolerance) #271
---
+++ 2024/03.py
@@ -0,0 +1,33 @@
+import re
+
+file="03_data.txt"
+
+def sum_muls(memory_string:str)->int:
+  suma_muls=0
+  mulregex = re.compile(r"mul\(\d{1,3},\d{1,3}\)")
+  muls = mulregex.finditer(memory_string)
+  for mul in muls:
+    nums = mul.group()[4:-1].split(",")
+    suma_muls += int(nums[0]) * int(nums[1])
+  return suma_muls
+
+
+with open(file, "r") as f:
+  mul_memory = f.read()
+  resultat = sum_muls(mul_memory)
+  # Mirem els límits entre un don't() i un do() i n'eliminem aquesta part de la memòria
+  dodonotregex = re.compile(r"don't\(\)(.|\n)*?do\(\)")
+  dolimits = dodonotregex.finditer(mul_memory)
+  offset = 0
+  for limit in dolimits:
+    mul_memory = mul_memory[:limit.start()-offset] + mul_memory[limit.end()-offset:]
+    offset += limit.end() - limit.start()
+  # Eliminem des de l'últim don't() fins al final, en cas d'existir. Tots els don't han sigut eliminats. Només pot quedar un don't() que no tingués un do() a continuació.
+  last_dont = list(re.finditer(r"don't\(\)", mul_memory))
+  if len(last_dont) > 0:
+    mul_memory = mul_memory[:last_dont[-1].start()]
+    print(last_dont[-1])
+  resultat2 = sum_muls(mul_memory)
+
+print("La suma dels muls és:", resultat)           # 180233229
+print("La suma dels muls permesos és:", resultat2) # 95411583
