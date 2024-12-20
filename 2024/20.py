4f43296ac0aa855213ed2f521a29d11ec2978f01
Oriol Sastre <oriol.sastre@gmail.com>
2024-12-20 15:11:25 +0100

    20

--- 2024/20.py
+++ 2024/20.py
@@ -17,8 +17,6 @@ def importa_mapa(file:str)->Mapa:
 def inicia_mapa_scores(mapa:Mapa)->MapaScore:
   mapa_scores = [[math.inf for _ in range(len(mapa[0]))] for _ in range(len(mapa))]
   return mapa_scores
-# def crear_mapa_revers(mapa:Mapa, inici: pos, final: pos)->Mapa:
-
 def posicio_inici_final(mapa:Mapa)->tuple[pos,pos]:
   inici=(-1,-1)
   final=(-1,-1)
@@ -41,6 +39,9 @@ def imprimeix_mapa_scores(mapa_scores:MapaScore, mapa:Mapa)->None:
         else: print(".", end="")
       else: print(int(mapa_scores[y][x])%10, end="")
     print("")
+def imprimeix_trampes(trampes:dict)->None:
+  estalvis = sorted(trampes.keys())
+  for estalvi in estalvis: print(estalvi, len(trampes[estalvi]), trampes[estalvi])
 def busca_camins(mapa:Mapa, mapa_scores:MapaScore, posicio:pos)->list[pos]:
   coords_veins=set(((-1,0),(0,1),(1,0),(0,-1)))
   (y,x)=posicio
@@ -83,21 +84,39 @@ def quantitat_trampes(trampes:dict, score_min:int)->int:
     if score >= score_min:
       quantitat+=len(trampes[score])
   return quantitat
-def busca_trampes_llargues(mapa:Mapa, mapa_scores:MapaScore, mida_trampa:int, estalvi_min:int)->int:
-  max_y, max_x = [len(mapa), len(mapa[0])]
-  for y1 in range(max_y-20):
-    for x1 in range(max_x-20):
-      for y2 in range(y1+2,y1+20):
-        for x2 in range(x1+2,x1+20):
-          if mapa[y1][x1]=="#" or mapa[y2][x2]=="#": continue
-          if abs(mapa_scores[y1][x1]-mapa_scores[y2][x2])-2 >= estalvi_min:
-            # Fer mini mapa on paret és cami i camí paret i calcular-ne l'score
-            a=0
-  return 0
+def determina_trampa(mapa_scores:MapaScore, a:pos, b:pos, mida_trampa:int, estalvi_min:int, trampes:dict)->None:
+  inici = a if mapa_scores[a[0]][a[1]]<mapa_scores[b[0]][b[1]] else b
+  final = b if inici == a else a
+  (y1,x1)=inici
+  (y2,x2)=final
+  trampa_feta=abs(y1-y2)+abs(x1-x2)
+  # Tenir en compte l'score de les parets que travesso!!!
+  score_diff=abs(mapa_scores[y1][x1]-mapa_scores[y2][x2])-trampa_feta
+  if score_diff >= estalvi_min and trampa_feta <= mida_trampa:
+      afegeix_trampa(trampes,score_diff,[(y1,x1),(y2,x2)])
+def busca_trampes_llargues(mapa:Mapa, mapa_scores:MapaScore, mida_trampa:int, estalvi_min:int)->dict:
+  trampes={}
+  [max_y, max_x] = [len(mapa), len(mapa[0])]
+  for y1 in range(max_y):
+    for x1 in range(max_x):
+      if mapa[y1][x1] == "#": continue
+      for y2 in range(y1,max_y):
+        if y2 > y1+mida_trampa: continue
+        for x2 in range(max_x):
+          if y1==y2 and x2<x1: continue
+          if x2 > x1+mida_trampa: continue
+          if mapa[y2][x2]=="#": continue
+          determina_trampa(mapa_scores, (y1,x1),(y2,x2),mida_trampa, estalvi_min,trampes)
+  return trampes
 
 mapa=importa_mapa(file)
 mapa_scores=inicia_mapa_scores(mapa)
 calcula_score_mapa(mapa, mapa_scores)
+
+estalvi1=100
 trampes=busca_trampes(mapa, mapa_scores)
-print("Hi ha", quantitat_trampes(trampes, 100), "trampes que t'estalvien un mínim de 100 picosegons.") # 1395
-# busca_trampes_llargues(mapa,mapa_scores,20,50)
+print("Hi ha", quantitat_trampes(trampes, estalvi1), "trampes que t'estalvien un mínim de", estalvi1, "picosegons.") # 1395
+
+estalvi2=100
+trampes2=busca_trampes_llargues(mapa,mapa_scores,20,estalvi2)
+print("Hi ha", quantitat_trampes(trampes2, estalvi2), "trampes llargues que t'estalvien un mínim de", estalvi2, "picosegons.") # 993178
