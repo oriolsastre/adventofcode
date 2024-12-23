17d4aa505702bf96b29b4feefdb09cae18de1846
Oriol Sastre Rienitz <oriol.sastre@gmail.com>
2024-12-23 14:08:00 +0100

    23

--- 2024/23.py
+++ 2024/23.py
@@ -24,8 +24,27 @@ def get_three_group(graph:Graph, first_letter:str="")->set[frozenset[str]]:
                 if node_c==node_a: continue
                 if node_c in veins_a: group.add(frozenset([node_a,node_b,node_c]))
     return group
-
+# https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm
+def find_maximal_cliques(graph:Graph):
+    max_clique=set()
+    def bron_kerbosch(r:set, p:set, x:set)->None:
+        if len(p)==0 and len(x)==0: max_clique.add(frozenset(r))
+        else:
+            for vertex in p:
+                bron_kerbosch(r.union({vertex}), p.intersection(graph[vertex]), x.intersection(graph[vertex]))
+                p=p.difference({vertex})
+                x=x.union({vertex})
+    bron_kerbosch(set(), set(graph.keys()), set())
+    return max_clique
+def find_biggest_clique(max_cliques:set[frozenset])->frozenset[str]:
+    return max(max_cliques, key=len)
+def get_clique_password(clique:frozenset[str])->str:
+    return ",".join(sorted(clique))
 
 graph = import_graph(file)
 grup_tres_test = get_three_group(graph,"t")
 print("Hi ha", len(grup_tres_test), "groups de 3 ordinadors en què almenys un comença per 't'.") # 1062
+
+maximal_cliques = find_maximal_cliques(graph)
+biggest_clique = find_biggest_clique(maximal_cliques)
+print("El password de la LAN party més grossa és:", get_clique_password(biggest_clique)) # bz,cs,fx,ms,oz,po,sy,uh,uv,vw,xu,zj,zm
