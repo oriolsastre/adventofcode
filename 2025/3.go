79d708d2610e5d19ecbb5d8ba1daea77286f4d7a
Oriol Sastre Rienitz <oriol.sastre@gmail.com>
2025-12-03 12:45:52 +0100

    3 2

--- 2025/3.go
+++ 2025/3.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -10,38 +11,38 @@ func day3() {
 	testInput := false
 	input := getInput(2025, 3, testInput, false)
 	inputArray := input2LineArray(input)
-	joltageSum := 0
+	joltageSum2 := 0
+	joltageSum12 := 0
 	for _, line := range inputArray {
-		joltage := findBiggestJoltage(line)
-		joltageSum += joltage
+		joltage2 := findBiggestJoltage(line, 2)
+		joltageSum2 += joltage2
+		joltage12 := findBiggestJoltage(line, 12)
+		joltageSum12 += joltage12
 	}
 	// 17100
-	fmt.Printf("Solució del 1er problema: %d\n", joltageSum)
+	fmt.Printf("Solució del 1er problema: %d\n", joltageSum2)
+	// 170418192256861
+	fmt.Printf("Solució del 2n problema: %d\n", joltageSum12)
 }
 
-func findBiggestJoltage(bank string) int {
+func findBiggestJoltage(bank string, batteries int) int {
 	bank = strings.TrimSpace(bank)
-	var bigL int
-	var bigR int
-	var indexL int
-	for i := range len(bank) - 1 {
-		bankIint, _ := strconv.Atoi(string(bank[i]))
-		if bankIint > bigL {
-			bigL = bankIint
-			indexL = i
+	var joltage = 0
+	leftIndex := 0
+	for battery := range batteries {
+		var maxJoltage int
+		for i := leftIndex; i < len(bank)-(batteries-battery-1); i++ {
+			batteryJoltage, _ := strconv.Atoi(string(bank[i]))
+			if batteryJoltage > maxJoltage {
+				maxJoltage = batteryJoltage
+				leftIndex = i + 1
+			}
+			if maxJoltage == 9 {
+				break
+			}
 		}
-		if bigL == 9 {
-			break
-		}
+		joltage += maxJoltage * int(math.Pow10(batteries-battery-1))
 	}
-	for j := range len(bank) - indexL - 1 {
-		bankJint, _ := strconv.Atoi(string(bank[len(bank)-j-1]))
-		if bankJint > bigR {
-			bigR = bankJint
-		}
-		if bigR == 9 {
-			break
-		}
-	}
-	return bigL*10 + bigR
+
+	return joltage
 }
